eead57b87c6dd63184a895bfe4b52dd95056e1ba
cipi <ciprian.manea2@student.upt.ro>
2022-05-21 16:48:56 +0300

    MODIM-47 MODIM-45 Added the list of the sellers intruments

--- src/main/java/org/loose/fis/sre/controllers/SellerController.java
+++ src/main/java/org/loose/fis/sre/controllers/SellerController.java
@@ -1,27 +1,45 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableView;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
 import org.loose.fis.sre.exceptions.*;
+import org.loose.fis.sre.model.Inst;
 import org.loose.fis.sre.model.Instrument;
+import org.loose.fis.sre.model.User;
 import org.loose.fis.sre.services.InstrService;
-import org.loose.fis.sre.services.UserService;
 
-import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class SellerController {
+public class SellerController implements Initializable {
+    @FXML
+    private TableColumn<Instrument, String> tableName = new TableColumn<>();
+    @FXML
+    private TableColumn<Instrument, String> tableCat = new TableColumn<>();;
+    @FXML
+    private TableColumn<Instrument, String> tablePrice = new TableColumn<>();;
+    @FXML
+    private TableColumn<Instrument, String> tableDesc = new TableColumn<>();;
     @FXML
     private TableView<Instrument> Table = new TableView<>();
+    @FXML
+    private TableView<Instrument> Table1 = new TableView<Instrument>();
     @FXML
     private TextField InstrName;
     @FXML
@@ -42,9 +60,36 @@ public class SellerController {
     @FXML
     private String InstrBuyer;
     @FXML
-    private Button SignOutSeller, Home, Sell, Delete, Review, History;
+    private Button SignOutSeller, Home, Sell, Delete, Review, History, myInst;
     private Stage stage;
     private Parent root;
+
+    ObservableList<Instrument> instr = FXCollections.observableArrayList();
+    private ArrayList<Instrument> list = new ArrayList<>();
+
+
+    /**
+     * @param url
+     * @param resourceBundle
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        tableName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableCat.setCellValueFactory(new PropertyValueFactory<>("category"));
+        tablePrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        tableDesc.setCellValueFactory(new PropertyValueFactory<>("descr"));
+        Table1.setItems(getInstruments());
+        //Table1.getColumns().addAll(tableName, tableCat, tablePrice, tableDesc);
+    }
+    private ObservableList<Instrument> getInstruments()  {
+        for (Instrument in : InstrService.GetRepository().find())
+            if(in.getSeller().equals(LoginController.getDenUser())) {
+                list.add(in);
+            }
+
+        instr.addAll(list);
+        return instr;
+    }
 
     public void handleAddAction() {
         Instrument i = new Instrument();
@@ -124,6 +169,14 @@ public class SellerController {
             this.stage.setScene(scene);
             this.stage.show();
         }
+        if(event.getSource() == myInst){
+            this.root = (Parent)FXMLLoader.load(getClass().getClassLoader().getResource("MyInstruments.fxml"));
+            this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(this.root);
+            this.stage.setTitle("My Instruments");
+            this.stage.setScene(scene);
+            this.stage.show();
+        }
     }
 
     public static void display() {
@@ -145,4 +198,6 @@ public class SellerController {
         Stage stage = (Stage) SignOutSeller.getScene().getWindow();
         stage.close();
     }
+
+
 }
--- src/main/java/org/loose/fis/sre/model/Inst.java
+++ src/main/java/org/loose/fis/sre/model/Inst.java
@@ -3,25 +3,27 @@ package org.loose.fis.sre.model;
 import org.dizitart.no2.objects.Id;
 import java.util.Objects;
 
-public class Instrument extends Inst {
+public class Inst {
+
+
     @Id
     private String name;
     private String category;
+
+
+
     private String descr;
     private String price;
-    private String buyer;
-    private String seller;
 
-    public Instrument(String name, String category, String descr, String price, String buyer, String seller){
+
+    public Inst(String name, String category, String descr, String price){
         this.name = name;
         this.category = category;
         this.descr = descr;
         this.price = price;
-        this.buyer = buyer;
-        this.seller = seller;
     }
 
-    public Instrument(){}
+    public Inst(){}
 
     public String getName() {
         return name;
@@ -53,32 +55,18 @@ public class Instrument extends Inst {
     public void setPrice(String price) {
         this.price = price;
     }
-    public String getBuyer() {
-        return buyer;
-    }
 
-    public String getSeller() {
-        return seller;
-    }
-
-    public void setBuyer(String buyer) {
-        this.buyer = buyer;
-    }
-
-    public void setSeller(String seller) {
-        this.seller = seller;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Instrument inst = (Instrument) o;
-        return Objects.equals(name, inst.name) && Objects.equals(category, inst.category) && Objects.equals(descr, inst.descr) && Objects.equals(price, inst.price) && Objects.equals(buyer, inst.buyer) && Objects.equals(seller, inst.seller);
+        Inst inst = (Inst) o;
+        return Objects.equals(name, inst.name) && Objects.equals(category, inst.category) && Objects.equals(descr, inst.descr) && Objects.equals(price, inst.price);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, category, descr, price, buyer, seller);
+        return Objects.hash(name, category, descr, price);
     }
 }
